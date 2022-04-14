e8cf5d296a52d4b1aaa019f1586165b4db16214d
alexeyinn <alexey.i.nn@gmail.com>
2022-04-14 10:40:29 +0300

    feat: User model and module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { User } from "./users/users.model";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
@@ -18,9 +18,10 @@ import { AppService } from "./app.service";
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
