83c7bd8f57042071b67ed8a2a45fb6ce59d33e8c
alexeyinn <alexey.i.nn@gmail.com>
2022-04-29 16:10:33 +0300

    feat: user and roles creating pipes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-// 1:11:32
+import { ValidationPipe } from "./pipes/validation.pipe";
+// 1:22:00
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,8 @@ async function start() {
   SwaggerModule.setup("api/docs", app, document);
   // Доступ ко всем эндпоинтам, только для авторизованных пользователей
   // app.useGlobalGuards(JwtAuthGuard)
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => console.log(`Server started on port - ${PORT}`));
 }
--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class AddRoleDto {
   @ApiProperty({ example: "ADMIN", description: "Обозначение роли" })
+  @IsString({ message: "Должно быть строкой" })
   readonly value: string;
   @ApiProperty({ example: "1", description: "Id пользователя из базы" })
+  @IsNumber({}, { message: "Должно быть числом" })
   readonly userId: number;
 }
